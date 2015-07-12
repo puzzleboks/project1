25515720687e86e596cf2af866177198896a703a
lon phelps <puzzleboks@gmail.com>
2015-07-12 09:33:32 -0400

    added pseudo code to js file

---
+++ memory.js
@@ -0,0 +1,34 @@
+//shuffle
+  //create an array with image urls (doubled)
+  //when 'play' button is clicked, shuffle deck
+  //change 'play' button to 'start over'
+
+  // function shuffle(array) {
+  //   var m = array.length, t, i;
+  //
+  //   // While there remain elements to shuffle…
+  //   while (m) {
+  //
+  //     // Pick a remaining element…
+  //     i = Math.floor(Math.random() * m--);
+  //
+  //     // And swap it with the current element.
+  //     t = array[m];
+  //     array[m] = array[i];
+  //     array[i] = t;
+  //   }
+  //
+  //   return array;
+  // }
+
+//reveal
+  //when a card is clicked, reveal the card that equals that position in the array (replace placeholder?)
+  //when a second card is clicked, reveal that card
+
+//match
+  //if cards match, remove them from the board and update score
+  //if cards don't match, hide them again (replace with placeholder)
+
+//win
+  //when all the cards are removed from the board, announce the winner (player with the most points)
+  //change 'play' button to 'play again'
