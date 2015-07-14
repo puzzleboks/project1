449e3d4e7e8cf5052c1a796348f5adda6284440d
lon phelps <puzzleboks@gmail.com>
2015-07-14 14:28:03 -0400

    added resetGame function

--- memory.js
+++ memory.js
@@ -25,7 +25,7 @@ var choicePic = "";
 var choiceNum = 0;
 
 //var state = 0;
-var click = 0;
+//var click = 0;
 
 function shuffle(array) {
   var m = array.length, t, i;
@@ -40,6 +40,29 @@ function shuffle(array) {
   }
   return array;
 }
+
+function resetGame() {
+  score1Num = 0;
+  score2Num = 0;
+  player = 1;
+  state = 0;
+  game = 0;
+  boardState = [0,0,0,0,0,0,0,0,0,0];//0=back,1=face,2=removed
+  matchArray = [];
+  cardArray = [];
+  choicePic = "";
+  choiceNum = 0;
+  document.querySelector("#playTxt").innerHTML = "Play";
+  score1.innerHTML = score1Num;
+  score2.innerHTML = score2Num;
+  card[0].src
+  for (var i=0; i<imgUrlArray.length; i++) {
+    card[i].src = "images/cardBack.png";
+    card[i].style.visibility = "visible";
+  }
+  //change all cards back to cardBack
+  //shuffle deck
+}
 //click play button to shuffle deck and start game
 playBtn.addEventListener("click", function(){
   var shuffled = shuffle(imgUrlArray);
@@ -47,9 +70,11 @@ playBtn.addEventListener("click", function(){
     console.log(shuffled);
     game = 1;
     document.querySelector("#playTxt").innerHTML = "Start Over";
+    score1.style.color = 'green';
     startGame ();
   }else{
     game = 0;
+    resetGame();
     //console.log("start over");
     //return;
   }
@@ -60,11 +85,11 @@ function startGame(){
   board.addEventListener("click", revealCards, false);
   function revealCards(e) {
     if (game === 0) {
-      //game = 1;
       return;
     }else{//game === 1
       if (e.target !== e.currentTarget) {
         var targetId = parseInt(e.target.className);
+
         if (state === 0) {
           e.target.src = imgUrlArray[targetId];
           game = 1;
@@ -72,11 +97,11 @@ function startGame(){
           boardState[targetId] = 1;
           choicePic = e.target.src;
           choiceNum = targetId;
-          //matchArray.push(e.target.src);
-          //cardArray.push(targetId);
+          //console.log(boardState);
         }else if (state === 1) {
           if (boardState[targetId] === 0) {
             e.target.src = imgUrlArray[targetId];
+            //boardState[targetId] = 2;
             //delay
             var myDelay = setTimeout(function () {myTimer()}, 1000);
             function myTimer() {
@@ -84,28 +109,40 @@ function startGame(){
                 e.target.style.visibility = 'hidden';
                 card[choiceNum].style.visibility = 'hidden';
                 boardState[targetId] = 2;
+                boardState[choiceNum] = 2;
                 //update score
                 if (player === 1) {
                   score1Num = score1Num + 2;
                   score1.innerHTML = score1Num;
-
                 }else{
                   score2Num = score2Num + 2;
                   score2.innerHTML = score2Num;
                 }
+                if (score1Num + score2Num === imgUrlArray.length) {
+                  if (score2Num > score1Num){
+                    alert ("Player 2 Wins!");
+                  }else if (score1Num > score2Num){
+                    alert ("Player 1 Wins!");
+                  }else{
+                    alert ("It's a Tie!");
+                  }
+                  resetGame();
+                }
               }else{
                 player = 2;
+                score2.style.color = "green";
                 e.target.src = "images/cardBack.png";
                 card[choiceNum].src = "images/cardBack.png";
                 boardState[targetId] = 0;
+                boardState[choiceNum] = 0;
               }
               state = 0;
               clearTimeout(myDelay);
               return;
             }
-
+          }else{
+            return;
           }
-          return;
         }
       }
       e.stopPropagation();
