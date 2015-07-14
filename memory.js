103104a36b4a71493964e1cedbf70d623abc52ad
lon phelps <puzzleboks@gmail.com>
2015-07-14 11:21:02 -0400

    added delay function to js file

--- memory.js
+++ memory.js
@@ -12,12 +12,14 @@ var card = document.getElementById('board').getElementsByTagName("img");
 var board = document.querySelector("#board");
 var playBtn = document.querySelector(".playBtn");
 //var turn = "none";
-var player = 0;
+var player = 1;
 var state = 0;
 var game = 0;
-var boardState = [0,0,0,0,0,0,0,0,0,0];
+var boardState = [0,0,0,0,0,0,0,0,0,0];//0=back,1=face,2=removed
 var matchArray = [];
 var cardArray = [];
+var choicePic = "";
+var choiceNum = 0;
 //var state = 0;
 var click = 0;
 
@@ -37,13 +39,15 @@ function shuffle(array) {
 //click play button to shuffle deck and start game
 playBtn.addEventListener("click", function(){
   var shuffled = shuffle(imgUrlArray);
-  console.log(shuffled);
-  if (player === 0){//if play then shuffle
-    player = 1;
+  if (game === 0){
+    console.log(shuffled);
+    game = 1;
     document.querySelector("#playTxt").innerHTML = "Start Over";
     startGame ();
   }else{
-    console.log("start over");
+    game = 0;
+    //console.log("start over");
+    //return;
   }
 })
 //reveal
@@ -52,29 +56,47 @@ function startGame(){
   board.addEventListener("click", revealCards, false);
   function revealCards(e) {
     if (game === 0) {
+      //game = 1;
       return;
-    }else{
+    }else{//game === 1
       if (e.target !== e.currentTarget) {
         var targetId = parseInt(e.target.className);
         if (state === 0) {
-          
-          if (click < 2) {
-            //reveal card
-            click++
+          e.target.src = imgUrlArray[targetId];
+          game = 1;
+          state = 1;
+          boardState[targetId] = 1;
+          choicePic = e.target.src;
+          choiceNum = targetId;
+          //matchArray.push(e.target.src);
+          //cardArray.push(targetId);
+        }else if (state === 1) {
+          if (boardState[targetId] === 0) {
             e.target.src = imgUrlArray[targetId];
-            matchArray.push(e.target.src);
-            if (matchArray[0] === matchArray[1]){
-              console.log("test");
+            //delay
+            var myDelay = setTimeout(function () {myTimer()}, 1000);
+            function myTimer() {
+              if (e.target.src === choicePic){
+                e.target.style.visibility = 'hidden';
+                card[choiceNum].style.visibility = 'hidden';
+                boardState[targetId] = 2;
+                console.log ("working");
+              }
+              clearTimeout(myDelay);
             }
+
           }
+          return;
         }
-        //if cards match, remove them from the board and update score
-        //if cards don't match, hide them again (replace with placeholder)
       }
+      e.stopPropagation();
+      //if cards match, remove them from the board and update score
+      //if cards don't match, hide them again (replace with placeholder)
     }
-    e.stopPropagation();
   }
+
 }
+
 
 //win
 //when all the cards are removed from the board, announce the winner (player with the most points)
